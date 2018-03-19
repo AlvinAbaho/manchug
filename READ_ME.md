3736caf11917292dd158a1231e1e1fafb38875d3
VINARIA-PC\abaho <abahoalvin@gmail.com>
2018-03-19 16:13:37 +0300

    Added more stuff from assumpta

--- READ_ME.md
+++ READ_ME.md
@@ -1,1 +1,3 @@
-
+```Angular2html
+UMAAU Site
+```
---
+++ manchug/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+
+def must_be_empty(value):
+    if value:
+        raise forms.ValidationError('is not Empty')
+
+
+class ContactForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    verify_email = forms.EmailField(label='please verify your email address')
+    subject = forms.CharField()
+    suggestion = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(required=False,
+                               widget=forms.HiddenInput,
+                               label="Leave empty",
+                               # validators=[validators.MaxLengthValidator(0)],
+                               validators=[must_be_empty]
+                               )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data['email']
+        verify = cleaned_data.get('verify_email')
+
+        if email != verify:
+            raise forms.ValidationError(
+                "You need to enter the same email in both fields"
+            )
+
+    def send_email(self):
+        # mail_subject = 'MESSAGE FROM UMAAU WEBSITE: {}'.format(self.cleaned_data['subject'])
+        mail_subject = 'MESSAGE FROM UMAAU WEBSITE'
+        print("AFASASS")
+        message = render_to_string('membership/email.html', {
+            'first_name': self.cleaned_data['first_name'],
+            'last_name': self.cleaned_data['last_name'],
+            'msg': self.cleaned_data['suggestion'],
+            'mail': self.cleaned_data['email'],
+        })
+
+        email = EmailMessage(mail_subject, message, from_email='arms143mro@gmail.com',
+                             to=['abahoalvin@gmail.com'])
+
+        print("EMAIL MESSAGE: \n", email)
+
+        email.send()
