2c4ad59e03dcf233b2cb5b3fe50a19585bacf5e7
VINARIA-PC\abaho <abahoalvin@gmail.com>
2018-03-19 17:44:05 +0300

    Added whitenoise settings in deploy

--- manchug/deploy.py
+++ manchug/deploy.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from whitenoise.django import DjangoWhiteNoise
+
 
 from django.core.wsgi import get_wsgi_application
+
+
+path = "/home/AlvinAbaho/manchug/manchug"
+
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manchug.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
